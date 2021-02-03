337cb74ef66b82ee3aeb9b39991f3f19615873d2
Stephen-prog <stev.monye04@gmail.com>
2021-02-02 23:12:06 -1100

    github finder

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '440a6b605b3e572a2112';
+    this.client_secret = '36e565f2307c52f493991a7ff3409f5ee1a736ef';
+    this.repos_count = 7;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
